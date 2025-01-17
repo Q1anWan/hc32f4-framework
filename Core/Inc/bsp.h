002455a210fa0344436d5639f25ed5048424e82a
qianwan <qianwan233@foxmial.com>
2025-01-17 19:47:40 +0800

    add uart

---
+++ Core/Inc/bsp.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define BSP_XTAL_PORT                   (GPIO_PORT_H)
+#define BSP_XTAL_PIN                    (GPIO_PIN_00 | GPIO_PIN_01)
+
+#ifdef __cplusplus
+}
+#endif
---
+++ Core/Src/bsp.c
@@ -0,0 +1,146 @@
+#include "hc32f460.h"
+#include "hc32_ll.h"
+#include "bsp.h"
+
+
+void BSP_CLK_Init(void)
+{
+    stc_clock_xtal_init_t     stcXtalInit;
+    stc_clock_pll_init_t      stcMpllInit;
+
+    GPIO_AnalogCmd(BSP_XTAL_PORT, BSP_XTAL_PIN, ENABLE);
+    (void)CLK_XtalStructInit(&stcXtalInit);
+    (void)CLK_PLLStructInit(&stcMpllInit);
+
+    /* Set bus clk div. */
+    CLK_SetClockDiv(CLK_BUS_CLK_ALL, (CLK_HCLK_DIV1 | CLK_EXCLK_DIV2 | CLK_PCLK0_DIV1 | CLK_PCLK1_DIV2 | \
+                                      CLK_PCLK2_DIV4 | CLK_PCLK3_DIV4 | CLK_PCLK4_DIV2));
+
+    /* Config Xtal and enable Xtal */
+    stcXtalInit.u8Mode = CLK_XTAL_MD_OSC;
+    stcXtalInit.u8Drv = CLK_XTAL_DRV_ULOW;
+    stcXtalInit.u8State = CLK_XTAL_ON;
+    stcXtalInit.u8StableTime = CLK_XTAL_STB_2MS;
+    (void)CLK_XtalInit(&stcXtalInit);
+
+    /* MPLL config (XTAL / pllmDiv * plln / PllpDiv = 200M). */
+    stcMpllInit.PLLCFGR = 0UL;
+    stcMpllInit.PLLCFGR_f.PLLM = 1UL - 1UL;
+    stcMpllInit.PLLCFGR_f.PLLN = 50UL - 1UL;
+    stcMpllInit.PLLCFGR_f.PLLP = 2UL - 1UL;
+    stcMpllInit.PLLCFGR_f.PLLQ = 2UL - 1UL;
+    stcMpllInit.PLLCFGR_f.PLLR = 2UL - 1UL;
+    stcMpllInit.u8PLLState = CLK_PLL_ON;
+    stcMpllInit.PLLCFGR_f.PLLSRC = CLK_PLL_SRC_XTAL;
+    (void)CLK_PLLInit(&stcMpllInit);
+    /* Wait MPLL ready. */
+    while (SET != CLK_GetStableStatus(CLK_STB_FLAG_PLL)) {
+        ;
+    }
+
+    /* sram init include read/write wait cycle setting */
+    SRAM_SetWaitCycle(SRAM_SRAMH, SRAM_WAIT_CYCLE0, SRAM_WAIT_CYCLE0);
+    SRAM_SetWaitCycle((SRAM_SRAM12 | SRAM_SRAM3 | SRAM_SRAMR), SRAM_WAIT_CYCLE1, SRAM_WAIT_CYCLE1);
+
+    /* flash read wait cycle setting */
+    (void)EFM_SetWaitCycle(EFM_WAIT_CYCLE5);
+    /* 3 cycles for 126MHz ~ 200MHz */
+    GPIO_SetReadWaitCycle(GPIO_RD_WAIT3);
+    /* Switch driver ability */
+    (void)PWC_HighSpeedToHighPerformance();
+    /* Switch system clock source to MPLL. */
+    CLK_SetSysClockSrc(CLK_SYSCLK_SRC_PLL);
+    /* Reset cache ram */
+    EFM_CacheRamReset(ENABLE);
+    EFM_CacheRamReset(DISABLE);
+    /* Enable cache */
+    EFM_CacheCmd(ENABLE);
+}
+
+uint8_t m_au8RxBuf[128U];
+
+int32_t DMA_Config(void)
+{
+    int32_t i32Ret;
+    stc_dma_init_t stcDmaInit;
+    stc_dma_llp_init_t stcDmaLlpInit;
+    stc_irq_signin_config_t stcIrqSignConfig;
+    static stc_dma_llp_descriptor_t stcLlpDesc;
+
+    /* DMA&AOS FCG enable */
+    FCG_Fcg0PeriphClockCmd(FCG0_PERIPH_DMA1, ENABLE);
+    // FCG_Fcg0PeriphClockCmd(FCG0_PERIPH_DMA2, ENABLE);
+    FCG_Fcg0PeriphClockCmd(FCG0_PERIPH_AOS, ENABLE);
+
+    /* USART_RX_DMA */
+    (void)DMA_StructInit(&stcDmaInit);
+    stcDmaInit.u32IntEn = DMA_INT_ENABLE;
+    stcDmaInit.u32BlockSize = 1UL;
+    stcDmaInit.u32TransCount = ARRAY_SZ(m_au8RxBuf);
+    stcDmaInit.u32DataWidth = DMA_DATAWIDTH_8BIT;
+    stcDmaInit.u32DestAddr = (uint32_t)m_au8RxBuf;
+    stcDmaInit.u32SrcAddr = (uint32_t)(&USART_UNIT->RDR);
+    stcDmaInit.u32SrcAddrInc = DMA_SRC_ADDR_FIX;
+    stcDmaInit.u32DestAddrInc = DMA_DEST_ADDR_INC;
+    i32Ret = DMA_Init(RX_DMA_UNIT, RX_DMA_CH, &stcDmaInit);
+    if (LL_OK == i32Ret) {
+        (void)DMA_LlpStructInit(&stcDmaLlpInit);
+        stcDmaLlpInit.u32State = DMA_LLP_ENABLE;
+        stcDmaLlpInit.u32Mode  = DMA_LLP_WAIT;
+        stcDmaLlpInit.u32Addr  = (uint32_t)&stcLlpDesc;
+        (void)DMA_LlpInit(RX_DMA_UNIT, RX_DMA_CH, &stcDmaLlpInit);
+
+        stcLlpDesc.SARx   = stcDmaInit.u32SrcAddr;
+        stcLlpDesc.DARx   = stcDmaInit.u32DestAddr;
+        stcLlpDesc.DTCTLx = (stcDmaInit.u32TransCount << DMA_DTCTL_CNT_POS) | (stcDmaInit.u32BlockSize << DMA_DTCTL_BLKSIZE_POS);;
+        stcLlpDesc.LLPx   = (uint32_t)&stcLlpDesc;
+        stcLlpDesc.CHCTLx = stcDmaInit.u32SrcAddrInc | stcDmaInit.u32DestAddrInc | stcDmaInit.u32DataWidth |  \
+                            stcDmaInit.u32IntEn      | stcDmaLlpInit.u32State    | stcDmaLlpInit.u32Mode;
+
+        DMA_ReconfigLlpCmd(RX_DMA_UNIT, RX_DMA_CH, ENABLE);
+        DMA_ReconfigCmd(RX_DMA_UNIT, ENABLE);
+        AOS_SetTriggerEventSrc(RX_DMA_RECONF_TRIG_SEL, RX_DMA_RECONF_TRIG_EVT_SRC);
+
+        stcIrqSignConfig.enIntSrc = RX_DMA_TC_INT_SRC;
+        stcIrqSignConfig.enIRQn  = RX_DMA_TC_IRQn;
+        stcIrqSignConfig.pfnCallback = &RX_DMA_TC_IrqCallback;
+        (void)INTC_IrqSignIn(&stcIrqSignConfig);
+        NVIC_ClearPendingIRQ(stcIrqSignConfig.enIRQn);
+        NVIC_SetPriority(stcIrqSignConfig.enIRQn, DDL_IRQ_PRIO_DEFAULT);
+        NVIC_EnableIRQ(stcIrqSignConfig.enIRQn);
+
+        AOS_SetTriggerEventSrc(RX_DMA_TRIG_SEL, RX_DMA_TRIG_EVT_SRC);
+
+        DMA_Cmd(RX_DMA_UNIT, ENABLE);
+        DMA_TransCompleteIntCmd(RX_DMA_UNIT, RX_DMA_TC_INT, ENABLE);
+        (void)DMA_ChCmd(RX_DMA_UNIT, RX_DMA_CH, ENABLE);
+    }
+
+    /* USART_TX_DMA */
+    (void)DMA_StructInit(&stcDmaInit);
+    stcDmaInit.u32IntEn = DMA_INT_ENABLE;
+    stcDmaInit.u32BlockSize = 1UL;
+    stcDmaInit.u32TransCount = ARRAY_SZ(m_au8RxBuf);
+    stcDmaInit.u32DataWidth = DMA_DATAWIDTH_8BIT;
+    stcDmaInit.u32DestAddr = (uint32_t)(&USART_UNIT->TDR);
+    stcDmaInit.u32SrcAddr = (uint32_t)m_au8RxBuf;
+    stcDmaInit.u32SrcAddrInc = DMA_SRC_ADDR_INC;
+    stcDmaInit.u32DestAddrInc = DMA_DEST_ADDR_FIX;
+    i32Ret = DMA_Init(TX_DMA_UNIT, TX_DMA_CH, &stcDmaInit);
+    if (LL_OK == i32Ret) {
+        stcIrqSignConfig.enIntSrc = TX_DMA_TC_INT_SRC;
+        stcIrqSignConfig.enIRQn  = TX_DMA_TC_IRQn;
+        stcIrqSignConfig.pfnCallback = &TX_DMA_TC_IrqCallback;
+        (void)INTC_IrqSignIn(&stcIrqSignConfig);
+        NVIC_ClearPendingIRQ(stcIrqSignConfig.enIRQn);
+        NVIC_SetPriority(stcIrqSignConfig.enIRQn, DDL_IRQ_PRIO_DEFAULT);
+        NVIC_EnableIRQ(stcIrqSignConfig.enIRQn);
+
+        AOS_SetTriggerEventSrc(TX_DMA_TRIG_SEL, TX_DMA_TRIG_EVT_SRC);
+
+        DMA_Cmd(TX_DMA_UNIT, ENABLE);
+        DMA_TransCompleteIntCmd(TX_DMA_UNIT, TX_DMA_TC_INT, ENABLE);
+    }
+
+    return i32Ret;
+}
